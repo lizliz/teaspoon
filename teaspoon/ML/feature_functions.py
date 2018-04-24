c7e7ed7946ad0eb9b55a8158862f35cf9ea5fd75
Liz Munch <emunch@albany.edu>
2018-04-24 10:34:06 -0400

    Debugging the bary_diff_matrix function

--- teaspoon/ML/feature_functions.py
+++ teaspoon/ML/feature_functions.py
@@ -6,6 +6,7 @@
 #
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 # -------------------------------------------- #
 # -------------------------------------------- #
@@ -95,156 +96,178 @@ def tent(Dgm, params, type = 'BirthDeath'):
 
 # coverts subscripts to indices
 def sub2ind(array_shape, rows, cols):
-    ind = rows*array_shape[1] + cols
-    ind[ind < 0] = -1
-    ind[ind >= array_shape[0]*array_shape[1]] = -1
-    return ind
+	ind = rows*array_shape[1] + cols
+	ind[ind < 0] = -1
+	ind[ind >= array_shape[0]*array_shape[1]] = -1
+	return ind
 
 
 # convert indices to subscripts
 def ind2sub(array_shape, ind):
-    ind[ind < 0] = -1
-    ind[ind >= array_shape[0]*array_shape[1]] = -1
-    rows = (ind.astype('int') / array_shape[1])
-    cols = ind % array_shape[1]
-    return rows, cols
+	ind[ind < 0] = -1
+	ind[ind >= array_shape[0]*array_shape[1]] = -1
+	rows = (ind.astype('int') / array_shape[1])
+	cols = ind % array_shape[1]
+	return rows, cols
 
 
 # find the quadrature/interpolation weights for common orthognal functions
 # define the function blocks
 # Chebyshev-Gauss points of the first kind
 def quad_cheb1(npts=10):
-    # get the Chebyshev nodes of the first kind
-    x = np.cos(np.pi * np.arange(0, npts+1) / npts)
-    
-    # get the corresponding quadrature weights
-    w = np.pi / (1 + np.repeat(npts, npts + 1)) 
-    
-    return x, w
+	# get the Chebyshev nodes of the first kind
+	x = np.cos(np.pi * np.arange(0, npts+1) / npts)
+	
+	# get the corresponding quadrature weights
+	w = np.pi / (1 + np.repeat(npts, npts + 1)) 
+	
+	return x, w
 
 
 # Computes the Legendre-Gauss-Lobatto nodes, and their quadrate weights.
 # The LGL nodes are the zeros of (1-x**2)*P'_N(x), wher P_N is the nth Legendre
 # polynomial
 def quad_legendre(npts=10):
-    # Truncation + 1
-    nptsPlusOne = npts + 1
-    eps = np.spacing(1)  # find epsilon, the machine precision
-    
-    # Use the Chebyshev-Gauss-Lobatto nodes as the first guess
-    x = np.cos(np.pi* np.arange(0, npts+1) / npts)
-    
-    # Get the Legendre Vandermonde Matrix
-    P = np.zeros((nptsPlusOne, nptsPlusOne))
-    
-    # Compute P_(N) using the recursion relation
-    # Compute its first and second derivatives and 
-    # update x using the Newton-Raphson method.
-    
-    xold = 2
-    
-    while np.max(np.abs(x-xold)) > eps:           
-        xold = x
-            
-        P[:, 0] = 1
-        P[:, 1] = x
-        
-        for k in range(1, npts):
-            P[:, k+1] = ((2 * k + 1) * x * P[:, k] - k * P[:, k-1] ) / (k+1)
-            
-        x = xold - ( x * P[:, npts] - P[:, npts-1] ) \
-        / ( nptsPlusOne * P[:, npts] )
+	# Truncation + 1
+	nptsPlusOne = npts + 1
+	eps = np.spacing(1)  # find epsilon, the machine precision
+	
+	# Use the Chebyshev-Gauss-Lobatto nodes as the first guess
+	x = np.cos(np.pi* np.arange(0, npts+1) / npts)
+	
+	# Get the Legendre Vandermonde Matrix
+	P = np.zeros((nptsPlusOne, nptsPlusOne))
+	
+	# Compute P_(N) using the recursion relation
+	# Compute its first and second derivatives and 
+	# update x using the Newton-Raphson method.
+	
+	xold = 2
+	
+	while np.max(np.abs(x-xold)) > eps:           
+		xold = x
+			
+		P[:, 0] = 1
+		P[:, 1] = x
+		
+		for k in range(1, npts):
+			P[:, k+1] = ((2 * k + 1) * x * P[:, k] - k * P[:, k-1] ) / (k+1)
+			
+		x = xold - ( x * P[:, npts] - P[:, npts-1] ) \
+		/ ( nptsPlusOne * P[:, npts] )
 
-    # get the corresponding quadrature weights
-    w = 2 / (npts * nptsPlusOne * P[:, npts] ** 2)
-    
-    return x, w
-    
+	# get the corresponding quadrature weights
+	w = 2 / (npts * nptsPlusOne * P[:, npts] ** 2)
+	
+	return x, w
+	
 
 # map the inputs to the function blocks
 # you can invoke the desired function block using:
 # quad_pts_and_weights['name_of_function'](npts)
 quad_pts_and_weights = {'cheb1' : quad_cheb1,
-           'legendre' : quad_legendre
-           }
+		   'legendre' : quad_legendre
+		   }
 
 
 # find the barycentric interplation weights
 def bary_weights(x):
-    # Calculate Barycentric weights for the base points x.
-    #
-    # Note: this algorithm may be numerically unstable for high degree
-    # polynomials (e.g. 15+). If using linear or Chebyshev spacing of base
-    # points then explicit formulae should then be used. See Berrut and
-    # Trefethen, SIAM Review, 2004.
+	# Calculate Barycentric weights for the base points x.
+	#
+	# Note: this algorithm may be numerically unstable for high degree
+	# polynomials (e.g. 15+). If using linear or Chebyshev spacing of base
+	# points then explicit formulae should then be used. See Berrut and
+	# Trefethen, SIAM Review, 2004.
 
-    # find the length of x
-    n = x.size
+	# find the length of x
+	n = x.size
 
-    # Rescale for numerical stability
-    eps = np.spacing(1)  # find epsilon, the machine precision
-    x = x * (1 / np.prod(x[x > -1 + eps] + 1)) ** (1/n)
+	# Rescale for numerical stability
+	eps = np.spacing(1)  # find epsilon, the machine precision
+	x = x * (1 / np.prod(x[x > -1 + eps] + 1)) ** (1/n)
 
-    # find the weights
-    w = np.zeros((1, n))
-    for i in np.arange(n):
-        w[0, i] = np.prod(x[i] - np.delete(x, i))
+	# find the weights
+	w = np.zeros((1, n))
+	for i in np.arange(n):
+		w[0, i] = np.prod(x[i] - np.delete(x, i))
 
-    return 1 / w
+	return 1 / w
 
 
 def bary_diff_matrix(xnew, xbase, w=None):
-    # Calculate both the derivative and plain Lagrange interpolation matrix
-    # using Barycentric formulae from Berrut and Trefethen, SIAM Review, 2004.
-    # xnew     Interpolation points
+	print('xnew:',xnew)
+	print('xbase:',xbase)
+	print('xnew shape is: {}\t xbase shape is{}'.format(xnew.shape, xbase.shape))
+	# Calculate both the derivative and plain Lagrange interpolation matrix
+	# using Barycentric formulae from Berrut and Trefethen, SIAM Review, 2004.
+	# xnew     Interpolation points
 
-    # xbase    Base points for interpolation
-    # w        Weights calculated for base points (optional)
+	# xbase    Base points for interpolation
+	# w        Weights calculated for base points (optional)
 
-    # if w is not set, set it
-    if w is None:
-        w = bary_weights(xbase)
+	# if w is not set, set it
+	if w is None:
+		w = bary_weights(xbase)
 
-    # get the length of the base points
-    n = xbase.size
+	# get the length of the base points
+	n = xbase.size
 
-    # get the length of the requested points
-    nn = xnew.size
+	# get the length of the requested points
+	nn = xnew.size
 
-    # replicate the weights vector into a matrix
-    wex = np.tile(w, (nn, 1))
+	# replicate the weights vector into a matrix
+	wex = np.tile(w, (nn, 1))
 
-    # Barycentric Lagrange interpolation (from Berrut and Trefethen, SIAM Review, 2004)
-    xdiff = np.tile(xnew[np.newaxis].T, (1, n)) - np.tile(xbase, (nn, 1))
+	# Barycentric Lagrange interpolation (from Berrut and Trefethen, SIAM Review, 2004)
+	xdiff = np.tile(xnew[np.newaxis].T, (1, n)) - np.tile(xbase, (nn, 1))
 
-    M = wex / xdiff
+	return xdiff
+	
+	M = wex / xdiff
 
-    divisor = np.tile(np.sum(M, axis=1)[np.newaxis].T, (1, n))
-    divisor[np.isinf(divisor)] = float("inf")
+	divisor = np.tile(np.sum(M, axis=1)[np.newaxis].T, (1, n))
+	divisor[np.isinf(divisor)] = float("inf")
 
-    M[np.isinf(M)] = float("inf")
-    M = M / divisor
+	M[np.isinf(M)] = float("inf")
+	M = M / divisor
 
-    M[np.isnan(M)] = 0
-    M[xdiff == 0] = 1
+	M[np.isnan(M)] = 0
+	M[xdiff == 0] = 1
 
-    # Construct the derivative (Section 9.3 of Berrut and Trefethen)
-    xdiff2 = xdiff ** 2
+	# Construct the derivative (Section 9.3 of Berrut and Trefethen)
+	xdiff2 = xdiff ** 2
 
-    frac1 = wex / xdiff
-    frac1[np.isinf(frac1)] = float("inf")
-    frac2 = wex / xdiff2
-    DM = (M * np.tile(np.sum(frac2, axis=1)[np.newaxis].T, (1, n)) - frac2) / \
-         np.tile(np.sum(frac1, axis=1)[np.newaxis].T, (1, n))
-    row, col = np.where(xdiff == 0)
+	frac1 = wex / xdiff
+	frac1[np.isinf(frac1)] = float("inf")
+	frac2 = wex / xdiff2
+	DM = (M * np.tile(np.sum(frac2, axis=1)[np.newaxis].T, (1, n)) - frac2) / \
+		 np.tile(np.sum(frac1, axis=1)[np.newaxis].T, (1, n))
+	print(xdiff.min())
+	plt.hist(xdiff.flatten())
+	plt.show()
+	# plt.imshow(xdiff)
+	# plt.colorbar()
+	# plt.show()
+	row, col = np.where(xdiff == 0)
 
-    if np.all(row == 0):  # or, row.size == 0:
-        DM[row, ] = (wex[row, ] / np.tile(w[col].T, (1, n))) / xdiff[row, ]
-        idx = sub2ind(DM.shape, row, col)
-        DM[idx] = 0
-        DM[idx] = -np.sum(DM[row, ], axis=1)
+	if np.all(row == 0):  # or, row.size == 0:
+		# print('Shapes: ')
+		# TestA = wex[row, ]  
+		# TestB = np.tile(w[col].T, (1, n)).T
+		# TestC = xdiff[row, ]
+		# print('TestA:', np.shape(TestA))
+		# print('TestB:', np.shape(TestB))
+		# print('TestC:', np.shape(TestC))
+		DM[row, ] = (wex[row, ] / np.tile(w[col].T, (1, n)).T) / xdiff[row, ]
+		idx = sub2ind(DM.shape, row, col)
+		DM[idx] = 0
+		# print('DM:',DM)
+		# print('row:',row)
+		# print('col:',col)
+		# print('DM[row,]:',DM[row,])
+		DM[idx] = -np.sum(DM[row, ], axis=1)
 
-    return DM, M
+	return DM, M
 
 
 ## Extracts the weights on the interpolation mesh using barycentric Lagrange interpolation.
@@ -262,7 +285,7 @@ def interp_polynomial(Dgm, params, type='BirthDeath'):
 	
 #	jacobi_func = params.jacobi_func
 	# check if we asked for a squre mesh or not
-	if type(params.d) == int:
+	if isinstance(params.d,int):
 		nx = params.d
 		ny = params.d
 	else:
@@ -281,7 +304,7 @@ def interp_polynomial(Dgm, params, type='BirthDeath'):
 	else:
 		print('Your choices for type are "BirthDeath" or "BirthLifetime".')
 		print('Exiting...')
-	return
+		return
 
 	# get the query points. xq are the brith times, yq are the death times.
 	xq, yq = A[:, 0], A[:, 1]
