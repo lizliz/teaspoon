2f46e9b55fec469724ef972df2d9223fcaff4049
EGR\myersau3 <myersau3@msu.edu>
2020-10-30 13:47:06 -0400

    recreated test pypi page starting with version 0.9.0.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="teaspoon", # Replace with your own username
-    version="1.0.5",
+    version="0.9.0",
     author="Elizabeth Munch and Firas Khasawneh",
     author_email="muncheli@msu.edu",
     description="A Topological Signal Processing Package",
