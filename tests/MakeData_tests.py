cb9c9cbe46bbda84eed028cda5f12c23d808c51a
Liz Munch <muncheli@msu.edu>
2021-07-30 12:00:20 -0400

    Getting closer to getting unittests running

--- tests/MakeData_tests.py
+++ tests/MakeData_tests.py
@@ -7,64 +7,79 @@ import teaspoon.MakeData.DynSysLib.DynSysLib as DSL
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-system = 'rossler'
-dynamic_state = 'periodic'
-t, ts = DSL.DynamicSystems(system, dynamic_state)
-
-TextSize = 15
-plt.figure(figsize = (12,4))
-gs = gridspec.GridSpec(1,2)
-
-ax = plt.subplot(gs[0, 0])
-plt.xticks(size = TextSize)
-plt.yticks(size = TextSize)
-plt.ylabel(r'$x(t)$', size = TextSize)
-plt.xlabel(r'$t$', size = TextSize)
-plt.plot(t,ts[0], 'k')
-
-ax = plt.subplot(gs[0, 1])
-plt.plot(ts[0], ts[1],'k.')
-plt.plot(ts[0], ts[1],'k', alpha = 0.25)
-plt.xticks(size = TextSize)
-plt.yticks(size = TextSize)
-plt.xlabel(r'$x(t)$', size = TextSize)
-plt.ylabel(r'$y(t)$', size = TextSize)
-
-plt.show()
 
 
 
 
+import unittest
 
-import teaspoon.MakeData.DynSysLib.DynSysLib as DSL
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 
-system = 'rossler'
-UserGuide = True
-L, fs, SampleSize = 1000, 20, 2000
-# the length (in seconds) of the time series, the sample rate, and the sample size of the time series of the simulated system.
-parameters = [0.1, 0.2, 13.0] # these are the a, b, and c parameters from the Rossler system model.
-InitialConditions = [1.0, 0.0, 0.0] # [x_0, y_0, x_0]
-t, ts = DSL.DynamicSystems(system, dynamic_state, L, fs, SampleSize, parameters,  InitialConditions, UserGuide)
+class Rossler(unittest.TestCase):
 
-TextSize = 15
-plt.figure(figsize = (12,4))
-gs = gridspec.GridSpec(1,2)
+    def test_Rossler(self):
 
-ax = plt.subplot(gs[0, 0])
-plt.xticks(size = TextSize)
-plt.yticks(size = TextSize)
-plt.ylabel(r'$x(t)$', size = TextSize)
-plt.xlabel(r'$t$', size = TextSize)
-plt.plot(t,ts[0], 'k')
 
-ax = plt.subplot(gs[0, 1])
-plt.plot(ts[0], ts[1],'k.')
-plt.plot(ts[0], ts[1],'k', alpha = 0.25)
-plt.xticks(size = TextSize)
-plt.yticks(size = TextSize)
-plt.xlabel(r'$x(t)$', size = TextSize)
-plt.ylabel(r'$y(t)$', size = TextSize)
+        system = 'rossler'
+        dynamic_state = 'periodic'
+        t, ts = DSL.DynamicSystems(system, dynamic_state)
 
-plt.show()
+        TextSize = 15
+        plt.figure(figsize = (12,4))
+        gs = gridspec.GridSpec(1,2)
+
+        ax = plt.subplot(gs[0, 0])
+        plt.xticks(size = TextSize)
+        plt.yticks(size = TextSize)
+        plt.ylabel(r'$x(t)$', size = TextSize)
+        plt.xlabel(r'$t$', size = TextSize)
+        plt.plot(t,ts[0], 'k')
+
+        ax = plt.subplot(gs[0, 1])
+        plt.plot(ts[0], ts[1],'k.')
+        plt.plot(ts[0], ts[1],'k', alpha = 0.25)
+        plt.xticks(size = TextSize)
+        plt.yticks(size = TextSize)
+        plt.xlabel(r'$x(t)$', size = TextSize)
+        plt.ylabel(r'$y(t)$', size = TextSize)
+
+        # plt.show()
+        print('Ran Rossler in MakeData_tests')
+
+    def test_Rossler2(self):
+
+
+
+        system = 'rossler'
+        UserGuide = True
+        L, fs, SampleSize = 1000, 20, 2000
+        # the length (in seconds) of the time series, the sample rate, and the sample size of the time series of the simulated system.
+        parameters = [0.1, 0.2, 13.0] # these are the a, b, and c parameters from the Rossler system model.
+        InitialConditions = [1.0, 0.0, 0.0] # [x_0, y_0, x_0]
+        t, ts = DSL.DynamicSystems(system, dynamic_state, L, fs, SampleSize, parameters,  InitialConditions, UserGuide)
+
+        TextSize = 15
+        plt.figure(figsize = (12,4))
+        gs = gridspec.GridSpec(1,2)
+
+        ax = plt.subplot(gs[0, 0])
+        plt.xticks(size = TextSize)
+        plt.yticks(size = TextSize)
+        plt.ylabel(r'$x(t)$', size = TextSize)
+        plt.xlabel(r'$t$', size = TextSize)
+        plt.plot(t,ts[0], 'k')
+
+        ax = plt.subplot(gs[0, 1])
+        plt.plot(ts[0], ts[1],'k.')
+        plt.plot(ts[0], ts[1],'k', alpha = 0.25)
+        plt.xticks(size = TextSize)
+        plt.yticks(size = TextSize)
+        plt.xlabel(r'$x(t)$', size = TextSize)
+        plt.ylabel(r'$y(t)$', size = TextSize)
+
+        # plt.show()
+        print('Ran Rossler2 in MakeData_tests')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_classification.py
+++ tests/test_classification.py
@@ -17,80 +17,92 @@ from scipy import stats
 import matplotlib.pyplot as plt
 from sklearn.linear_model import RidgeCV, RidgeClassifierCV
 
-# to reload a module
-from imp import reload
-reload(fF)
-reload(gpc)
-
-# ----------------------------------
-# define a parameters bucket
-# ----------------------------------
-params = ParameterBucket()
-
-# ----------------------------------
-# generate a data frame for testing
-# ----------------------------------
-
-# --- Test classification
-# df = gpc.testSetClassification()
-# dgmColLabel = 'Dgm'
-# params.clfClass = RidgeClassifierCV
+import unittest
 
 
-# --- Test regression
-# df = gpc.testSetRegressionBall()
-# dgmColLabel = 'Dgm'
-# params.clfClass = RidgeCV
+
+class TestManifolds(unittest.TestCase):
+
+    def test_learning_Manifolds(self):
+
+        # ----------------------------------
+        # define a parameters bucket
+        # ----------------------------------
+        params = ParameterBucket()
+
+        # ----------------------------------
+        # generate a data frame for testing
+        # ----------------------------------
+
+        # --- Test classification
+        # df = gpc.testSetClassification()
+        # dgmColLabel = 'Dgm'
+        # params.clfClass = RidgeClassifierCV
 
 
-# --- Test manifold classification
-df = gpc.testSetManifolds(numDgms=50, numPts=100)  # numpTs=200
-# dgmColLabel = ['Dgm0', 'Dgm1']
-dgmColLabel = ['Dgm1']
-params.clfClass = RidgeClassifierCV
-params.TF_Learning = False
-#print('\n', params, '\n')
-
-# get a diagram
-#Dgm = df['Dgm'][0]
-
-# ----------------------------------
-# Find bounding box
-# ----------------------------------
-params.setBoundingBox(df[dgmColLabel], pad=.05)
-params.jacobi_poly = 'cheb1'  # choose the interpolating polynomial
-params.useAdaptivePart = False
-# ----------------------------------
-# define the number of base points
-# ----------------------------------
-params.d = 10
+        # --- Test regression
+        # df = gpc.testSetRegressionBall()
+        # dgmColLabel = 'Dgm'
+        # params.clfClass = RidgeCV
 
 
-# ----------------------------------
-# choose a feature function
-# ----------------------------------
-params.feature_function = fF.interp_polynomial
-#params.feature_function = fF.tent
+        # --- Test manifold classification
+        df = gpc.testSetManifolds(numDgms=50, numPts=100)  # numpTs=200
+        # dgmColLabel = ['Dgm0', 'Dgm1']
+        dgmColLabel = ['Dgm1']
+        params.clfClass = RidgeClassifierCV
+        params.TF_Learning = False
+        #print('\n', params, '\n')
 
-# ----------------------------------
-# Run the experiment
-# ----------------------------------
-num_runs = 10
-yy = np.zeros((num_runs))
-for i in np.arange(num_runs):
-    xx = getPercentScore(df,
-                         labels_col='trainingLabel',
-                         dgm_col=dgmColLabel,
-                         params=params,
-                         normalize=False,
-                         verbose=False
-                         )
-    yy[i] = xx['score']
+        # get a diagram
+        #Dgm = df['Dgm'][0]
 
-print('\navg success rate = {}\nStdev = {}'.format(np.mean(yy), np.std(yy)))
+        # ----------------------------------
+        # Find bounding box
+        # ----------------------------------
+        params.setBoundingBox(df[dgmColLabel], pad=.05)
+        params.jacobi_poly = 'cheb1'  # choose the interpolating polynomial
+        params.useAdaptivePart = False
+        # ----------------------------------
+        # define the number of base points
+        # ----------------------------------
+        params.d = 10
 
-kernel = stats.gaussian_kde(yy)
-values = np.linspace(yy.min(), yy.max(), 1000)
-# plotting
-#plt.plot(values, kernel(values), '.')
-# plt.show()
+
+        # ----------------------------------
+        # choose a feature function
+        # ----------------------------------
+        params.feature_function = fF.interp_polynomial
+        #params.feature_function = fF.tent
+
+        # ----------------------------------
+        # Run the experiment
+        # ----------------------------------
+        num_runs = 10
+        yy = np.zeros((num_runs))
+        for i in np.arange(num_runs):
+            xx = getPercentScore(df,
+                                 labels_col='trainingLabel',
+                                 dgm_col=dgmColLabel,
+                                 params=params,
+                                 normalize=False,
+                                 verbose=False
+                                 )
+            yy[i] = xx['score']
+
+        print('\navg success rate = {}\nStdev = {}'.format(np.mean(yy), np.std(yy)))
+
+        kernel = stats.gaussian_kde(yy)
+        values = np.linspace(yy.min(), yy.max(), 1000)
+        # plotting
+        #plt.plot(values, kernel(values), '.')
+        # plt.show()
+
+
+        print('Tested mainfold learning ')
+        self.assertEqual(0, 1, "Need to check the manifold learning ")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
